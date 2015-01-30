2297be7a0999258abbe9f3073e1a1064006596d4
leonardoDVC <leonardodvc@leonardodvc-ThinkPad-T60.(none)>
2015-01-30 09:31:06 +0700

    signup in out finish

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+  def new
+  	@user=User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Japanese Elearning System"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password,
+                                   :password_confirmation)
+    end
+end
