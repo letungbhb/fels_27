538cd2743d599da9ff74ba196a0ac6f1408f69ad
leonardoDVC <leonardodvc@leonardodvc-ThinkPad-T60.(none)>
2015-01-30 11:30:01 +0700

    fix indent code signup login logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   def index
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
   	@user=User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,22 @@
 module SessionsHelper
-	#dang nhap
-	def log_in(user)
-		session[:user_id]=user.id
-	end
-	#dang xuat
-	def log_out
-		session.delete(:user_id)
-		@current_user=nil
-	end
-	#tra lai nguoi dung dang dang nhap
-	def current_user
-     	@current_user ||= User.find_by(id: session[:user_id])
-  	end
-  	#tra lai gia tri true neu da dang nhap,false neu chua dang nhap
-  	def logged_in?
-  		!current_user.nil?
-  	end
+  #dang nhap
+  def log_in(user)
+    session[:user_id]=user.id
+  end
+
+  #dang xuat
+  def log_out
+	session.delete(:user_id)
+    @current_user=nil
+  end
+
+  #tra lai nguoi dung dang dang nhap
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  #tra lai gia tri true neu da dang nhap,false neu chua dang nhap
+  def logged_in?
+    !current_user.nil?
+  end
 end
