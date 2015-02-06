59faca8e03248f73873969e8a66acd7397106ac2
leonardoDVC <leonardodvc@leonardodvc-ThinkPad-T60.(none)>
2015-02-06 17:34:07 +0700

    user-signup-login-logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,32 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
+  
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password,
+                                   :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "U dont have right to access this page.Please log in."
+        redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @user = User.find params[:id]
+    if current_user.id != @user.id
+      flash[:danger] = "U dont have right to access this page.Please log in."
+      redirect_to root_url
+    end
+  end
+
+  def correct_supervisor
+    if !current_user.is_supervisor?
+      flash[:danger] = "U are not admin.U dont have right to access this page."
+      redirect_to root_url
+      end
+    end
 end
